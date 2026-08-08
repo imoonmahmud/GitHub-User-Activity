58edd1825a451f6e97857160986d9c8b6b5dcc07
imoonmahmud <imoonmahmud@gmail.com>
2026-08-08 18:28:29 +0600

    Created two files the request file contain the API program and the other one is the main program

---
+++ main.py
@@ -0,0 +1,29 @@
+import argparse
+from request import fetch_events
+
+def format_event(event):
+    event_type = event['type']
+    match event_type:
+        case 'PushEvent':
+            return f'Pushed to {event['repo']['name']}'
+        case 'PullRequestEvent':
+            return f'Created pull request {event['payload']['pull_request']['number']}'
+        case 'CreateEvent':
+            return f'Created {event['payload']['ref_type']} {event['payload']['ref']}'
+        case 'IssueCommentEvent':
+            return f'Commeneted on issue {event['payload']['issue']['number']}'
+            
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username')
+    args = parser.parse_args()
+
+    events = fetch_events(args.username)
+    if events is None:
+        return
+
+    for event in events:
+        print(format_event(event))
+
+if __name__ == '__main__':
+    main()
---
+++ request.py
@@ -0,0 +1,18 @@
+import urllib.request
+import json
+
+def fetch_events(username):
+    url = f"https://api.github.com/users/{username}/events"
+    try:
+        response = urllib.request.urlopen(url)
+        return json.load(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            print(f'Error: user {username} not found.')
+        elif e.code == 403:
+            print('Error: GitHub API rate limit exceeded. Try again later.')
+        else:
+            print(f'Error: GitHub returned status {e.code}')
+        return None
+    except urllib.error.URLError as e:
+        print(f'Error: could not reach GitHub ({e.code})')
